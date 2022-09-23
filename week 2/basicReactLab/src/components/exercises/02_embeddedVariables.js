1fe64a22ff122d902ceeca97eccbc025b65af9ab
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2022-09-23 14:01:58 +0100

    Exercise 2 Complete

--- week 2/basicReactLab/src/components/exercises/02_embeddedVariables.js
+++ week 2/basicReactLab/src/components/exercises/02_embeddedVariables.js
@@ -3,8 +3,34 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
 const Demo = () => {
+  const noLectures = [2, 4];
+  const noPracticals = [4, 6];
+  const name = ["DevOps", "Web Development"];
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No. of Lectures</th>
+          <th>No. of Practicals</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>{name[0]}</td>
+          <td>{noLectures[0]}</td>
+          <td>{noPracticals[0]}</td>
+        </tr>
+
+        <tr>
+          <td>{name[1]}</td>
+          <td>{noLectures[1]}</td>
+          <td>{noPracticals[1]}</td>
+        </tr>
+      </tbody>
+    </table>
+
   );
 };
 
